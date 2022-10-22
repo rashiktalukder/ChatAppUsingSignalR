f7cca031484503339ea830cb5abbae3c2460a61b
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2022-10-22 21:01:18 +0600

    Chatting app done and working well..

--- BlazorChatSignalR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -4,11 +4,22 @@ namespace BlazorChatSignalR.Server.Hubs
 {
     public class ChatHub:Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        
         public override async Task OnConnectedAsync()
         {
-            await SendMessageToChat("", "User connected");
+            string userName=Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, userName);
+            await SendMessageToChat(string.Empty,$"{userName} connected!");
             await base.OnConnectedAsync();
         }
+
+        public override async Task OnDisconnectedAsync(Exception? ex)
+        {
+            string userName = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await SendMessageToChat(string.Empty,$"{userName} Left..");
+        }
+
         public async Task SendMessageToChat(string name, string message)
         {
             await Clients.All.SendAsync("ReceivedMessage",name, message);
